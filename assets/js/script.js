9e92a4a209ce72499e3e46ce29ee5bc5222035c2
John Vayianos <john.c.vayianos@gmail.com>
2022-02-03 20:27:30 -0500

    search history

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+document.createElement("li");
+var searchHistoryEl = document.createElement("li");
+searchHistoryEl.textContent = "hello";
+var searchListAdd = document.querySelector("#search-list-add");
+searchListAdd.appendChild(searchListAdd);
